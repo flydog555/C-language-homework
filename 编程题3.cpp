ddaab118fc144ccfe7c3a29a95b25e10dbf46ba4
flydog555 <160736311+flydog555@users.noreply.github.com>
2024-09-10 17:45:13 +0800

    Add files via upload

---
+++ 编程题3.cpp
@@ -0,0 +1,11 @@
+//#include <stdio.h>
+//#include <graphics.h>
+//#include <conio.h>
+//int main()
+//{
+//	initgraph(200, 200);
+//	rectangle(50, 50, 100, 100);
+//	ellipse(50, 50, 100, 100);
+//	_getch();
+//	closegraph();
+//}
