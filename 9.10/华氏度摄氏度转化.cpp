d64f28b660c8f1fe6dc90cf5869e460e11d19e5e
flydog555 <160736311+flydog555@users.noreply.github.com>
2024-09-10 17:48:51 +0800

    Add files via upload

---
+++ 9.10/华氏度摄氏度转化.cpp
@@ -0,0 +1,10 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//    float  fahr, celsius;
+//    scanf("%f", &fahr);  //输入华垔温度
+//    celsius = 5 * (fahr - 32) / 9;
+//    printf("celsius = %f\n", celsius);//输出摄垔温度
+//    return 0;
+//}
