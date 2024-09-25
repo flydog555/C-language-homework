f34f66cd7f71eb2c23b342a434728246753b1428
flydog555 <160736311+flydog555@users.noreply.github.com>
2024-09-25 10:57:40 +0800

    Update and rename 9.10/编程题4.cpp to 9.10/第一章自测题/编程题4.cpp

---
+++ 9.10/第一章自测题/编程题4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <graphics.h>
+#include <conio.h>
+#include <math.h>
+int main()
+{
+	initgraph(300, 300);
+	circle(100, 100, 80);
+	float cx = 2 * (sqrt(80 * 80 / 2));
+	float a = cx / 2;
+	rectangle(100-a, 100-a, 100+a, 100+a);
+	_getch();
+	closegraph();
+}
