017650a46006a35cc57aba779cb32fec4c04a757
flydog555 <160736311+flydog555@users.noreply.github.com>
2024-09-25 10:57:04 +0800

    Update and rename 9.10/编程题2.cpp to 9.10/第一章自测题/编程题2.cpp

---
+++ 9.10/第一章自测题/编程题2.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	printf("ÇëÊäÈëÁ½¸öÕýÕûÊý£º");
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d+%d=%d\n", a, b, a + b);
+	printf("%d-%d=%d\n", a, b, a - b);
+	printf("%d*%d=%d\n", a, b, a * b);
+	printf("%d/%d=%d\n", a, b, a / b);
+	return 0;
+}
